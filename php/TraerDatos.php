e0fce18e46f6f3a6ab52d1eb7da0244e1a492243
jakson leal <jleal338@unab.edu.co>
2020-04-07 15:59:32 -0500

    prueba master php

--- php/TraerDatos.php
+++ php/TraerDatos.php
@@ -1,10 +1,7 @@
 <?php
 
     $insmostar= 'SELECT latitud,longitud FROM coordenadas ORDER by id DESC LIMIT 1 ';
-    //echo $insmostar;
 
-
-    //echo $ins;
     define('DBHost', '127.0.0.1');
     define('DBName', 'basurapp');
     define('DBUser', 'root');
@@ -12,17 +9,5 @@
     require("pdo/src/PDO.class.php");
     $DB = new Db(DBHost, DBName, DBUser, DBPassword);
     $rta = $DB->query($insmostar);
-   
-    
-   // $id = $DB->lastInsertId();
      
     echo json_encode($rta[0]);
-
-
-
-
-//}
-
-
-
-
