a57af54f7042768cd5592c2064955f48e7806426
unknown <carlrey9>
2020-04-13 16:04:38 -0500

    creacion de 3 actividades: main, postlogin y registro, transicion entre ellas pero no se implementó nada de string

--- BasurApp/app/src/main/java/co/edu/unab/proyectomoviles/basurapp/MainActivity.java
+++ BasurApp/app/src/main/java/co/edu/unab/proyectomoviles/basurapp/MainActivity.java
@@ -2,13 +2,49 @@ package co.edu.unab.proyectomoviles.basurapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnIngresar ;
+    Button btnRegistrarse;
+    Button btnOmitir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        asociarElementos();
+
+        btnOmitir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getApplicationContext(), postlogin.class);
+                startActivity(in);
+                Toast.makeText(getApplicationContext(), "Bienvenido Pendejo", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btnRegistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getApplicationContext(), registro.class);
+                startActivity(in);
+                Toast.makeText(getApplicationContext(), "A registrarse weee", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+    }
+
+    public void asociarElementos(){
+        btnIngresar = findViewById(R.id.btn_ingresar);
+        btnRegistrarse = findViewById(R.id.btn_registrarse);
+        btnOmitir = findViewById(R.id.btn_omitir);
     }
 }
---
+++ BasurApp/app/src/main/java/co/edu/unab/proyectomoviles/basurapp/postlogin.java
@@ -0,0 +1,14 @@
+package co.edu.unab.proyectomoviles.basurapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class postlogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_postlogin);
+    }
+}
