511f79487c9f399e35c70e6af02543027b06ccdc
jakson leal <jleal338@unab.edu.co>
2020-05-01 22:52:08 -0500

    primer commit

--- BasurApp/app/src/main/java/co/edu/unab/proyectomoviles/basurapp/view/activity/MainActivity.java
+++ BasurApp/app/src/main/java/co/edu/unab/proyectomoviles/basurapp/view/activity/MainActivity.java
@@ -1,8 +1,12 @@
-package co.edu.unab.proyectomoviles.basurapp;
+package co.edu.unab.proyectomoviles.basurapp.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import co.edu.unab.proyectomoviles.basurapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
     }
 }
